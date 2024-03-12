c4f864dd5f30aa7acd29f11467c5d72b30efa230
arnav rustagi <arnav.rustagi@plaksha.edu.in>
2024-03-12 22:57:53 +0530

    update

--- data_preprocessing/synthesize.py
+++ data_preprocessing/synthesize.py
@@ -1,24 +1,30 @@
 from phonemedb import PhonemeDataBase
 from tts_hin import TTS
 
-def t2s_modification(prompt):
-    head = ""
-    tail = prompt
-    for char in prompt:
-        print(char, end=" ")
-        if char in " \n\r\t": 
+class Modifier(object):
+    def __init__(self): 
+        self.phonemedb = PhonemeDataBase()
+
+    def t2s_modification(self,prompt):
+        head = ""
+        tail = prompt
+        for char in prompt:
+            print(char, end=" ")
+            if char in " \n\r\t": 
+                head += tail[:1]
+                tail = tail[1:]
+                continue
+            phoneme = self.phonemedb.get_random_phoneme()
+            yield head + phoneme + tail[1:]
             head += tail[:1]
             tail = tail[1:]
-            continue
-        phoneme = PhonemeDataBase.get_random_phoneme()
-        yield head + phoneme + tail[1:]
-        head += tail[:1]
-        tail = tail[1:]
 
 if __name__ == "__main__":
     from tqdm import tqdm
-    modifications = list(t2s_modification("मेरे पास बीस बंदर हैं"))
+    modifier = Modifier()
+    modifications = list(modifier.t2s_modification("मेरे पास बीस बंदर हैं"))
     tts = TTS()
+    print("starting tts")
     for i,modified in tqdm(enumerate(modifications)):
-        tts(modified, f"{i}.wav")
+        tts(modified, f"./dump/{i}.wav")
 
--- data_preprocessing/tts_hin.py
+++ data_preprocessing/tts_hin.py
@@ -1,21 +1,15 @@
-from transformers import VitsModel, AutoTokenizer
-import torch
-from scipy.io.wavfile import write
+from typing import List
+from random import choice
+from models import MODELS 
+
 
 class TTS(object):
-    def __init__(self, model_checkpoint:str="facebook/mms-tts-hin", tokenizer_checkpoint:str="facebook/mms-tts-hin"):
-        self.model_checkpoint = model_checkpoint>
-        self.tokenizer_checkpoint = tokenizer_checkpoint
-
-        self.model = VitsModel.from_pretrained(self.model_checkpoint)
-        self.tokenizer = AutoTokenizer.from_pretrained(self.tokenizer_checkpoint)
+    def __init__(self, models:List[str]=MODELS, tokenizer_checkpoint:str="facebook/mms-tts-hin"):
+        self.models = models
 
     def __call__(self, text:str,filename:str="output.wav") -> None:
-        inputs = self.tokenizer(text, return_tensors="pt")
+        choice(self.models)(text, filename)
 
-        with torch.no_grad():
-            output = self.model(**inputs).waveform
-            write(filename, rate=self.model.config.sampling_rate, data=output.float().numpy().T)
 
 if __name__ == "__main__":
     tts = TTS()
