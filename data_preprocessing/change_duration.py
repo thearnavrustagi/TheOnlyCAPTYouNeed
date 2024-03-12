cb0eff5599de73d1fbfe0c9eeb9efed5b2092764
Satvik Bajpai <151377321+SatvikBajpai@users.noreply.github.com>
2024-03-12 23:30:42 +0530

    Add files via upload
    feature modification scripts

---
+++ data_preprocessing/change_duration.py
@@ -0,0 +1,24 @@
+import librosa
+import soundfile as sf
+
+def change_duration(input_audio_file, output_audio_file, duration_scaling_factor):
+    """
+    Change the duration of an audio file and save the resulting audio.
+    
+    Parameters:
+    input_audio_file (str): Path to the input audio file (in .wav format).
+    output_audio_file (str): Path to save the audio file with changed duration (in .wav format).
+    duration_scaling_factor (float): Scaling factor for changing the duration of the audio.
+    """
+    y, sr = librosa.load(input_audio_file)
+
+    y_stretched = librosa.effects.time_stretch(y, rate = duration_scaling_factor)
+
+    sf.write(output_audio_file, y_stretched, sr)
+
+    print("Audio with changed duration saved as:", output_audio_file)
+
+input_audio_file = 'download.wav'
+output_audio_file = 'output_audio_duration_changed.wav'
+duration_scaling_factor = 1.2
+change_duration(input_audio_file, output_audio_file, duration_scaling_factor)
