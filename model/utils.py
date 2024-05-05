7f1276300a6055fe786e8b8f5ac7c150518b7607
arnav rustagi <arnav.rustagi@plaksha.edu.in>
2024-05-05 13:58:45 +0530

    waow

--- model/utils.py
+++ model/utils.py
@@ -19,7 +19,7 @@ import os
 class MetricEvaluater(object):
     @staticmethod
     def create_metric_store():
-        return {"auc": [], "precision": [], "recall": [], "f1_score": []}
+        return {"auc": [], "precision": [], "recall": [], "f1_score": [], "accuracy"}
 
     @staticmethod
     def evaluate(metrics, *, y_pred, y, n_classes):
@@ -27,16 +27,15 @@ class MetricEvaluater(object):
             val = None
             match key:
                 case "auc":
-                    val = MetricEvaluater.compute_auc(y_pred, y, n_classes)
+                    metrics[key].append(MetricEvaluater.compute_auc(y_pred, y, n_classes)))
                 case "precision":
-                    val = MetricEvaluater.compute_precision(y_pred, y, n_classes)
+                    metrics[key].append(MetricEvaluater.compute_precision(y_pred, y, n_classes))
                 case "recall":
-                    pass
-                    val = MetricEvaluater.compute_recall(y_pred, y, n_classes)
+                    metrics[key].append(MetricEvaluater.compute_recall(y_pred, y, n_classes))
                 case "f1_score":
-                    pass
-                    val = MetricEvaluater.compute_f1_score(y_pred, y, n_classes)
-            metrics[key].append(val)
+                    metrics[key].append(MetricEvaluater.compute_f1_score(y_pred, y, n_classes))
+                case "accuracy":
+                    metrics[key].append(MetricEvaluater.compute_accuracy(y_pred, y, n_classes))
 
     @staticmethod
     def compute_auc(y_pred, y, n_classes):
@@ -74,6 +73,13 @@ class MetricEvaluater(object):
                 labels_prediction[i], labels_real[i], num_classes=n_classes
             ).item()
         return f1_scores / len(y_pred)
+
+    def compute_accuracy(y_pred, y, n_classes):
+        labels_prediction = torch.argmax(y_pred, axis=-1)
+        labels_real = torch.argmax(y, axis=-1)
+        precisions = 0
+        mask = labels_prediction == labels_real
+        return np.mean(mask.numpy())
 
 
 def one_hot_encode(index):
@@ -118,9 +124,10 @@ def train_one_epoch(
 
         if classes:
             MetricEvaluater.evaluate(metrics, y_pred=y_pred, y=y, n_classes=classes)
-            #status_text += f"f1_score: {mean(metrics['f1_score']):.4f}; "
-            #status_text += f"recall: {mean(metrics['recall']):.4f}; "
-            status_text += f"accuracy: {mean(metrics['precision']):.4f} "
+            status_text += f"f1_score: {mean(metrics['f1_score']):.4f}; "
+            status_text += f"recall: {mean(metrics['recall']):.4f}; "
+            status_text += f"precision: {mean(metrics['precision']):.4f} "
+            status_text += f"accuracy: {mean(metrics['accuracy']):.4f} "
         progress_bar.set_description(status_text)
 
     metrics["loss"] = running_loss
